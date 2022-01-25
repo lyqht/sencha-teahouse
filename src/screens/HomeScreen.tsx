d1b4ade95f879a72c0361eca58867b5fed4c03a8
lyqht <estee.tsw@gmail.com>
2022-01-26 01:24:18 +0800

    add styling to product item on home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,20 +1,37 @@
-import { FlatList, Text } from "native-base";
+import { AddIcon, Box, FlatList, HStack, IconButton, Image, Input, MinusIcon, Spacer, Text, VStack } from "native-base";
 import React from "react";
 import { SafeAreaView, StyleSheet, ViewStyle } from "react-native";
-import { Product, useStore } from "../store";
+import { useStore } from "../store";
+import { Product } from "../types/product";
 
 const HomeScreen: React.FC = () => {
     const products = useStore(state => state.products);
 
-
     const _renderItem = ({ item }: { item: Product }) => {
+        const { coverImage, name, price, quantity } = item;
+        const maxQuantity = quantity;
+
         return (
-            <Text>{item.name}</Text>
+            <HStack>
+                <Image size={"120"} source={{uri: coverImage}} alt={name} />
+                <VStack paddingX={4} paddingTop={4}>
+                    <Text fontSize={"md"}>{name}</Text>
+                    <Text fontSize={"xl"}>${price}</Text>
+                    <Spacer />
+                    <Text m={0} p={0}>{quantity}px left</Text>
+                </VStack>
+                <HStack position={"absolute"} right={0} bottom={0} mx={6} h={8}>
+                    <Spacer />
+                    <IconButton size={"md"} variant="solid" icon={<MinusIcon size="4" />}/>
+                    <Input mx="3" textAlign={"center"} w={12} placeholder="0" keyboardType="number-pad" />
+                    <IconButton size={"md"} variant="solid" icon={<AddIcon size="4" />} />
+                </HStack>
+            </HStack>
         );
     };
     return (
         <SafeAreaView style={styles.container}>
-            <FlatList keyExtractor={(item, index) => `${item}+${index}`} data={products} renderItem={_renderItem} />
+            <FlatList w={"full"} keyExtractor={(item, index) => `${item}+${index}`} data={products} renderItem={_renderItem} />
         </SafeAreaView>
     );
 };
