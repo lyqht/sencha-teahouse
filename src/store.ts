c48ba1379dffe1c8f389b5610077f95ffa33603c
lyqht <estee.tsw@gmail.com>
2022-01-27 00:21:16 +0800

    prettify graphql code

--- src/store.ts
+++ src/store.ts
@@ -1,40 +1,49 @@
-import { CMS_BASE_URL } from "@env";
-import request, { gql } from "graphql-request";
+import {CMS_BASE_URL} from "@env";
+import request, {gql} from "graphql-request";
 import create from "zustand";
-import { Product } from "./types/product";
+import {Product} from "./types/product";
 
 interface ProductState {
-    products: Product[],
-    addProduct: (product: Product) => void,
-    fetchProducts: () => void,
+    products: Product[];
+    addProduct: (product: Product) => void;
+    fetchProducts: () => void;
 }
 
 type TeaFromCMS = Omit<Product, "coverImage"> & {
-    coverImage: {url: string}
-}
+    coverImage: {url: string};
+};
 
 interface CMSResponse {
-    teas: TeaFromCMS[]
+    teas: TeaFromCMS[];
 }
 
 export const useStore = create<ProductState>(set => ({
     products: [],
-    addProduct: (newProduct: Product) => set(state => ({ products: [...state.products, newProduct] })),
+    addProduct: (newProduct: Product) =>
+        set(state => ({products: [...state.products, newProduct]})),
     fetchProducts: async () => {
         const query = gql`
-query MyQuery {
-    teas {
-    id
-    name
-    price
-    quantity
-    coverImage {
-        url
-    }
-    }
-}
-                `;
-        const { teas: fetchedProducts }: CMSResponse = await request(`${CMS_BASE_URL}`, query);
-        set({ products: fetchedProducts.map(product => ({...product, coverImage: product.coverImage.url})) });
+            query MyQuery {
+                teas {
+                    id
+                    name
+                    price
+                    quantity
+                    coverImage {
+                        url
+                    }
+                }
+            }
+        `;
+        const {teas: fetchedProducts}: CMSResponse = await request(
+            `${CMS_BASE_URL}`,
+            query,
+        );
+        set({
+            products: fetchedProducts.map(product => ({
+                ...product,
+                coverImage: product.coverImage.url,
+            })),
+        });
     },
 }));
