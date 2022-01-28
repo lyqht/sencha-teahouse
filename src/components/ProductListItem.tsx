1288f986393135ec6e3e11c147f6a23044a2a626
lyqht <estee.tsw@gmail.com>
2022-01-28 11:18:39 +0800

    added remove cart item button, and slight restyling

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -24,13 +24,13 @@ const ProductListItem: React.FC<Props> = ({ item }) => {
             </VStack>
             <HStack position={"absolute"} right={0} bottom={0} mx={6} h={8}>
                 <Spacer />
-                <IconButton size={"md"} variant={`${displayValue === 0 ? "disabled" : "solid"}`} icon={<MinusIcon size="4" />} isDisabled={displayValue === 0} onPress={() => {removeFromCart(item);}}/>
+                <IconButton size={"md"} variant={`${displayValue === 0 ? "disabled" : "outline"}`} icon={<MinusIcon size="4" />} isDisabled={displayValue === 0} onPress={() => {removeFromCart(item);}}/>
                 <Input mx="3" textAlign={"center"} w={12} keyboardType="number-pad" defaultValue="0" value={`${displayValue}`} onChangeText={(text) => {
                     const userInputQty = parseInt(text, 10);
                     if ( userInputQty < quantity) {
                         addToCartInQty(item, userInputQty);
                     } }} />
-                <IconButton size={"md"} variant={`${displayValue === quantity ? "disabled" : "solid"}`} icon={<AddIcon size="4" />} isDisabled={displayValue === quantity} onPress={() => { addToCart(item); }} />
+                <IconButton size={"md"} variant={`${displayValue === quantity ? "disabled" : "outline"}`} icon={<AddIcon size="4" />} isDisabled={displayValue === quantity} onPress={() => { addToCart(item); }} />
             </HStack>
         </HStack>
     );
